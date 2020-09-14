9432d43ef8554f4839c3e3be5429b20b0f74b326
annanl <annanl@student.chalmers.se>
2020-09-14 20:08:54 +0200

    get stories by id,

--- server/controllers/stories.js
+++ server/controllers/stories.js
@@ -1,26 +1,55 @@
 const Story = require('../models/Story');
 
+//create a story by the user
 const createStories = (req, res, next) => {
     var story = new Story(req.body);
-    story.save(function(err) {
+    Story.save(function(err) {
         if (err) { return next(err); }
         res.status(201).json(story);
-    });
-}
+    }).carch(error => {
+        res.status(500).json({ error: error });
+      });
+};
 
-const getAllStories = (req, res) => {
-    const options = {
+//get stories of a user with his ID
+const getStoriesById = (req, res) => {
+    const id = req.params.id;
+    Story.findById(id)
+    .select('-__v')//version key
+    .populate('user').exec().then(doc =>{ 
+        if (!doc) throw 404;
+    res.status(200).json({
+        id: doc.id,
+        unique_views:doc.unique_views,
+        lifespan_views:doc.lifespan,
+        upload_date: doc.upload_date,
+        likes: doc.likes,
+        request: [
+          {
+            type: 'GET',
+            url: 'http://localhost:3000/api/stories'
+          },
+          {
+            type: 'GET',
+            url: `http://localhost:3000/api/users/${doc.user}`
+          }
+        ]
+      }).catch(error=>{
+          if (error === 404)
+    res.status(404).json({error:'Story of the user not found.' });
+    else res.status(500).json({ error: error });
+});
+    })
 }
 
 
 
 module.exports = {
-  getAllStories,
-  getStoryWithId,
   createStories,
+  getStoriesById, 
   putStoryWithId,
   updateStoryById,
   deleteStories,
   deleteStoryWithId,
 };
-}
+
