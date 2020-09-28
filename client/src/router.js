c1ccc8b432ff48b488bb1e4a1cc4a7f852043352
annanl <annanl@student.chalmers.se>
2020-09-28 16:34:42 +0200

    Revert "Update router.js signup page"
    This reverts commit 79a778980a17a4745e4128adec930cb632cabf0c

--- client/src/router.js
+++ client/src/router.js
@@ -12,11 +12,6 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home
-    },
-    {
-      path: '/signup',
-      name: 'signup',
-      component: Signup
     }
   ]
 })
