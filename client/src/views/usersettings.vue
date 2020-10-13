0098273dc7f3a2cccfcd49be416462c7c37c71b0
annanl <annanl@student.chalmers.se>
2020-10-13 18:57:30 +0200

    Replace usersettings.vue

--- client/src/views/usersettings.vue
+++ client/src/views/usersettings.vue
@@ -1,8 +1,20 @@
 <template>
-  <div class="usersettings">
-    <usersettings />
+  <div>
+    <b-jumbotron >
+      <b-container >
+        <b-row align-h="center" align-v="center" class="mt-4">
+          <b-col cols="8" >
+            <b-card class="p-1">
+              <div class="usersettings"/>
+               <usersettings />
+            </b-card>
+          </b-col>
+        </b-row>
+      </b-container>
+    </b-jumbotron>
   </div>
 </template>
+
 <script>
 import usersettings from '../components/usersettings'
 export default {
@@ -11,3 +23,25 @@ export default {
   }
 }
 </script>
+
+<style>
+  .p-1 {
+    padding-right: 5rem;
+    background-color: lightblue
+  }
+  b-container{
+  background-color: lightblue
+  }
+body {
+  background-color: lightblue
+}
+.action {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+}
+img {
+border: 3px groove rgb(26, 0, 143);
+}
+</style>
