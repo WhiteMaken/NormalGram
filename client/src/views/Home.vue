1d5ddac1560e00bba21bd9353ea5cf70eb4f28fc
annanl <annanl@student.chalmers.se>
2020-10-14 13:14:56 +0200

    Make it responsive and log out will not disappear from the screen

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,49 +1,32 @@
 <template>
   <div>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <nav class="navbar navbar-expand-lg navbar-light bg-lightblue">
       <div class="container">
-        <a class="navbar-brand" href="#">Navbar</a>
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <a href='/pictures'>Pictures</a>
-        <a href='/users/settings'>Settings</a>
-        <a href='/myposts'>Posts</a>
-        <a href='/stories'>Story</a>
-
-        <div
-          class="collapse navbar-collapse justify-content-end"
-          id="navbarNav"
-        >
-          <ul class="navbar-nav">
-            <li class="nav-item active">
-              <a class="nav-link" @click="logUserOut"> Logout</a>
-            </li>
-          </ul>
-        </div>
+<div class="topnav">
+        <a href="/pictures">Pictures</a>
+        <a href="/users/settings">Settings</a>
+        <a href="/myposts">Posts</a>
+        <a href="/stories">Story</a>
+        <a class="nav-link" @click="getUserDetails"> Normalgram</a>
+        <c class="nav-link" @click="logUserOut"> Logout</c>
+</div>
+      </div>
+      <div>
       </div>
     </nav>
-    <section>
-      <div class="container mt-5">
-        <div class="row">
-          <div class="col-md-12">
-            <ul class="list-group">
-              <li class="list-group-item">Name : {{ user.name }}</li>
-              <li class="list-group-item">Email : {{ user.email }}</li>
-              <li class="list-group-item">Username : {{ user.username }}</li>
-            </ul>
-          </div>
-        </div>
-      </div>
-    </section>
+    <b-container>
+      <b-row align-h="center" align-v="center" class="mt-3">
+        <b-col cols="10">
+          <b-card class="p-1">
+            <div class="usersettings" />
+            <li class="list-group-item">Name : {{ user.name }}</li>
+            <li class="list-group-item">Email : {{ user.email }}</li>
+            <li class="list-group-item">Username : {{ user.username }}</li>
+            <usersettings />
+          </b-card>
+        </b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
 <script>
@@ -70,4 +53,61 @@ export default {
   }
 }
 </script>
-<style scoped></style>
+<style>
+.p-1 {
+  padding-right: 10rem;
+  background-color: lightblue;
+}
+
+/* Add a black background color to the top navigation */
+.topnav {
+  background-color: lightblue;
+  overflow: hidden;
+}
+.container{
+ background-color: lightblue
+}
+
+.topnav a {
+  float: left;
+  color: #042b75;
+  text-align: center;
+  padding: 14px 20px;
+  text-decoration: none;
+  font-size: 17px;
+}
+
+/* Change the color of links on hover */
+.topnav a:hover {
+  background-color: rgb(108, 235, 235);
+  color: black;
+}
+
+/* Add a color to the active/current link */
+.topnav a.active {
+  background-color: #4CAF50;
+  color: white;
+}
+
+.topnav c{ background-color: rgb(22, 60, 61);
+  overflow: hidden;
+}
+.topnav c {
+  float: right;
+  color: #f2f2f2;
+  text-align: right;
+  padding: 14px 20px;
+  text-decoration: none;
+  font-size: 17px;
+}
+
+.topnav c:hover {
+  background-color: brown;
+  color: black;
+}
+
+img {
+  border: 3px groove rgb(26, 0, 143);
+}
+
+</style>
